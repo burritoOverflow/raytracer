48d14a669d4a26b37e0124c6e2d13aa65b5ac2df
Sondre Kongsgård <sondre.kongsgaard@gmail.com>
2020-03-26 16:04:14 +0100

    Add example app to render a sphere with the Phong model

--- app/main.cpp
+++ app/main.cpp
@@ -3,6 +3,7 @@
 #include "Canvas.h"
 #include "Color.h"
 #include "Intersection.h"
+#include "Light.h"
 #include "Point.h"
 #include "Ray.h"
 #include "Sphere.h"
@@ -21,10 +22,15 @@ int main(void) {
   double half = wall_size / 2;
 
   raytracer::Canvas canvas(canvas_pixels, canvas_pixels);
-  raytracer::utility::Color color(1, 0, 0);
   raytracer::geometry::Sphere shape;
+  shape.material_.color_ = raytracer::utility::Color(1, 0.2, 1);
+
+  raytracer::utility::Point light_position(-10, 10, -10);
+  raytracer::utility::Color light_color(1, 1, 1);
+  raytracer::scene::PointLight light(light_position, light_color);
 
   double world_x, world_y;
+  raytracer::utility::Color color;
   for (size_t y = 0; y < canvas_pixels; ++y) {
     world_y = half - pixel_size * y;
     for (size_t x = 0; x < canvas_pixels; ++x) {
@@ -33,11 +39,18 @@ int main(void) {
       // Describe the point on the wall that the ray will target
       raytracer::utility::Point position(world_x, world_y, wall_z);
 
-      raytracer::utility::Ray r(ray_origin,
-                                (position - ray_origin).Normalize());
-      auto xs = raytracer::geometry::Intersect(shape, r);
+      raytracer::utility::Ray ray(ray_origin,
+                                  (position - ray_origin).Normalize());
+      auto xs = raytracer::geometry::Intersect(shape, ray);
+      auto hit = raytracer::geometry::Hit(xs);
 
-      if (raytracer::geometry::Hit(xs) != std::nullopt) {
+      if (hit != std::nullopt) {
+        raytracer::utility::Point point = ray.Position(hit.value().t_);
+        raytracer::utility::Vector normal = hit.value().object_.NormalAt(point);
+        raytracer::utility::Vector eye = -ray.direction_;
+
+        color = raytracer::scene::Lighting(hit.value().object_.material_, light,
+                                           point, eye, normal);
         canvas.WritePixel(x, y, color);
       }
     }
