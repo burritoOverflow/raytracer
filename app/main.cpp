edf0ffe291348f98759d1389d5e283e5c911b40e
Sondre Kongsgård <sondre.kongsgaard@gmail.com>
2020-05-20 11:44:22 +0200

    Add example scene with various patterns applied

--- app/main.cpp
+++ app/main.cpp
@@ -2,19 +2,27 @@
 
 #include "Camera.h"
 #include "Canvas.h"
+#include "CheckersPattern.h"
+#include "GradientPattern.h"
 #include "Matrix.h"
 #include "Plane.h"
+#include "RingPattern.h"
 #include "Sphere.h"
+#include "StripePattern.h"
 #include "World.h"
 
 int main(void) {
   raytracer::scene::World world;
 
-  // Add a floor as an extremely flattened sphere with a matte texture
+  // Add a floor with a matte, striped texture
   auto floor = std::make_shared<raytracer::geometry::Plane>();
   floor->material_ = raytracer::material::Material();
-  floor->material_.color_ = raytracer::utility::Color(1, 0.9, 0.9);
   floor->material_.specular_ = 0;
+  floor->material_.pattern_ =
+      std::make_shared<raytracer::pattern::StripePattern>(
+          raytracer::pattern::StripePattern(
+              raytracer::utility::Color(0, 0, 0),
+              raytracer::utility::Color(1, 1, 1)));
   world.objects_.push_back(floor);
 
   // Add a large unit sphere in the middle, translated upward slightly and
@@ -25,6 +33,13 @@ int main(void) {
   middle->material_.color_ = raytracer::utility::Color(0.204, 0.749, 0.286);
   middle->material_.diffuse_ = 0.7;
   middle->material_.specular_ = 0.3;
+  middle->material_.pattern_ =
+      std::make_shared<raytracer::pattern::GradientPattern>(
+          raytracer::pattern::GradientPattern(
+              raytracer::utility::Color(1, 0.298, 0.298),
+              raytracer::utility::Color(0.204, 0.749, 0.286)));
+  middle->material_.pattern_.value()->transform_ =
+      raytracer::utility::Translation(-0.5, 1, 0.5);
   world.objects_.push_back(middle);
 
   // Add a smaller blue sphere on the right
@@ -36,6 +51,12 @@ int main(void) {
   right->material_.color_ = raytracer::utility::Color(0, 0.60, 0.898);
   right->material_.diffuse_ = 0.7;
   right->material_.specular_ = 0.3;
+  right->material_.pattern_ = std::make_shared<raytracer::pattern::RingPattern>(
+      raytracer::pattern::RingPattern(
+          raytracer::utility::Color(0.204, 0.749, 0.286),
+          raytracer::utility::Color(0, 0.60, 0.898)));
+  right->material_.pattern_.value()->transform_ =
+      raytracer::utility::Identity().RotateX(M_PI_2).Scale(0.1, 0.1, 0.1);
   world.objects_.push_back(right);
 
   // Add an even smaller red sphere on the left
@@ -47,6 +68,13 @@ int main(void) {
   left->material_.color_ = raytracer::utility::Color(1, 0.298, 0.298);
   left->material_.diffuse_ = 0.7;
   left->material_.specular_ = 0.3;
+  left->material_.pattern_ =
+      std::make_shared<raytracer::pattern::CheckersPattern>(
+          raytracer::pattern::CheckersPattern(
+              raytracer::utility::Color(0, 0.60, 0.898),
+              raytracer::utility::Color(1, 0.298, 0.298)));
+  left->material_.pattern_.value()->transform_ =
+      raytracer::utility::Identity().Scale(0.25, 0.25, 1);
   world.objects_.push_back(left);
 
   // Add a white light source, shining from above and to the left
